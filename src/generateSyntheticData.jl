341eac7f479bfdbc6219642e7794ad609cfb832a
Nicholas Johnson <>
2021-09-22 21:02:50 -0400

    Commented data generation script

--- src/generateSyntheticData.jl
+++ src/generateSyntheticData.jl
@@ -1,5 +1,32 @@
 function generate_synthetic_data(n, param_frac, sigma; signal_to_noise=10,
     fixed_rank_sparse=false, fixed_rank=1, fixed_sparse=0)
+    """
+    This function samples synthetic data by adding noise to the sum of a
+    random low rank matrix and a random sparse matrix.
+
+    :param n: The row and column dimension of the data to be sampled (Int64).
+    :param param_frac: Parameter that controls the rank and sparsity of the
+                       sampled low rank and sparse matrix when
+                       fixed_rank_sparse=false (Float64).
+    :param sigma: Parameter that controls the absolute magnitude of the noise
+                  (Float64).
+    :param signal_to_noise: Parameter that controls the signal to noise ratio of
+                            the sampled data (Float64).
+    :param fixed_rank_sparse: If true, use fixed_rank and fixed_sparse as the
+                              rank and sparsity respectively of the low rank
+                              matrix and the sparse matrix to be sampled (Bool).
+    :param fixed_rank: Rank of the low rank matrix to be sampled when
+                       fixed_rank_sparse=true.
+    :param fixed_sparse: Sparsity of the sparse matrix to be sampled when
+                         fixed_rank_sparse=true
+
+    :return: This function returns 5 values:
+             1) The sampled n x n data matrix.
+             2) The sampled n x n low rank matrix.
+             3) The sampled n x n sparse matrix.
+             4) The sparsity level of the sparse matrix.
+             5) The rank of the low rank matrix.
+    """
 
     if fixed_rank_sparse
         k_sparse = fixed_sparse
@@ -9,11 +36,13 @@ function generate_synthetic_data(n, param_frac, sigma; signal_to_noise=10,
         k_rank = Int64(floor(param_frac * n))
     end
 
+    # Sample a rank k_rank matrix
     d = Normal(0, signal_to_noise * sigma / n^0.5)
     X = rand(d, (n, k_rank))
     Y = rand(d, (n, k_rank))
     L_0 = X * Y'
 
+    # Sample a sparsity k_sparse matrix
     d = Uniform(-5, 5)
     E = rand(d, (n, n))
     S_0 = zeros(n, n)
@@ -32,6 +61,7 @@ function generate_synthetic_data(n, param_frac, sigma; signal_to_noise=10,
 
     data = L_0 + S_0
 
+    # Add noise to the sum of the sampled low rank and sparse matrices
     d = Normal(0, sigma^2)
     noise = rand(d, (n, n))
     data = data + noise
@@ -42,6 +72,34 @@ end;
 
 function generate_synthetic_data_symmetric(n, param_frac, sigma;
     signal_to_noise=10, fixed_rank_sparse=false, fixed_rank=1, fixed_sparse=0)
+    """
+    This function samples symmetric synthetic data by adding symmetric noise to
+    the sum of a random symmetric low rank matrix and a random symmetric sparse
+    matrix.
+
+    :param n: The row and column dimension of the data to be sampled (Int64).
+    :param param_frac: Parameter that controls the rank and sparsity of the
+                       sampled low rank and sparse matrix when
+                       fixed_rank_sparse=false (Float64).
+    :param sigma: Parameter that controls the absolute magnitude of the noise
+                  (Float64).
+    :param signal_to_noise: Parameter that controls the signal to noise ratio of
+                            the sampled data (Float64).
+    :param fixed_rank_sparse: If true, use fixed_rank and fixed_sparse as the
+                              rank and sparsity respectively of the low rank
+                              matrix and the sparse matrix to be sampled (Bool).
+    :param fixed_rank: Rank of the low rank matrix to be sampled when
+                       fixed_rank_sparse=true.
+    :param fixed_sparse: Sparsity of the sparse matrix to be sampled when
+                         fixed_rank_sparse=true
+
+    :return: This function returns 5 values:
+             1) The sampled n x n data matrix.
+             2) The sampled n x n low rank matrix.
+             3) The sampled n x n sparse matrix.
+             4) The sparsity level of the sparse matrix.
+             5) The rank of the low rank matrix.
+    """
 
     if fixed_rank_sparse
         k_sparse = fixed_sparse
@@ -51,10 +109,12 @@ function generate_synthetic_data_symmetric(n, param_frac, sigma;
         k_rank = Int64(floor(param_frac * n))
     end
 
+    # Sample a rank k_rank symmetric matrix
     d = Normal(0, signal_to_noise * sigma / n^0.5)
     X = rand(d, (n, k_rank))
     L_0 = X * X'
 
+    # Sample a sparsity k_sparse symmetric matrix
     d = Uniform(-5, 5)
     E = rand(d, (n, n))
     S_0 = zeros(n, n)
@@ -79,6 +139,7 @@ function generate_synthetic_data_symmetric(n, param_frac, sigma;
 
     data = L_0 + S_0
 
+    # Add symmetric noise to the sum of the sampled low rank and sparse matrices
     d = Normal(0, sigma^2)
     noise = rand(d, (n, n))
     for i = 1:(n-1)
