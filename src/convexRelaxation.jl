f7c8e8e45979bcc766759465b7a128b80080880c
Nicholas Johnson <>
2021-09-22 22:48:24 -0400

    Added docstring for convex relaxation

--- src/convexRelaxation.jl
+++ src/convexRelaxation.jl
@@ -1,11 +1,8 @@
-# TODO: edit function description to describe zero_indices and one_indices and
-# round_relaxed_sol params and return vals
 function solve_perspective_relaxation(U, mu, lambda, k_sparse, k_rank;
-    zero_indices=[], one_indices=[], solver_output=0, check_feasibility=true,
-    round_relaxed_sol=false, sdp_solver="Mosek")
+    zero_indices=[], one_indices=[], solver_output=0)
     """
     This function exactly solves the perspective reformulation relaxation of the
-    problem of interest and also rounds this lower bound to a feasible solution.
+    problem of interest.
 
     :param U: An arbitrary n-by-n matrix.
     :param mu: The regularization parameter for the sparse matrix penalty
@@ -14,14 +11,22 @@ function solve_perspective_relaxation(U, mu, lambda, k_sparse, k_rank;
                    (Float64).
     :param k_sparse: The maximum sparsity of the sparse matrix (Int64).
     :param k_rank: The maximum rank of the low rank matrix (Int64).
+    :param zero_indices: List of 2-tuples of Int64 where each entry consists of
+                         an index (i, j) such that Y_ij is constrained to take
+                         value 0.
+    :param one_indices: List of 2-tuples of Int64 where each entry consists of
+                        an index (i, j) such that S_ij is constrained to take
+                        value 1 where S is the binary matrix denoting the
+                        sparsity pattern of Y.
+    :param solver_output: Solver_output param to be passed to Mosek (Int64).
 
-    :return: This function returns two values. The first value is a tuple of two
+    :return: This function returns two values. The first value is a tuple of 5
              n-by-n arrays that correspond to the solution of the perspective
-             relaxation (the first element in the tuple is the matrix X and the
-             second element is the matrix Y). The second value is a tuple of
-             two n-by-n arrays that correspond to the rounded feasible
-             solution to the original problem (the first element in the tuple
-             is the matrix X and the second element is the matrix Y).
+             relaxation (the first element in the tuple is the matrix X, the
+             second element is the matrix Y, the third element is the matrix Z,
+             the fourth element is the matrix P and the fifth element is the
+             matrix Theta). The second value is the optimal objective value of
+             the relaxation (Float64).
     """
     n = size(U)[1]
 
@@ -32,13 +37,8 @@ function solve_perspective_relaxation(U, mu, lambda, k_sparse, k_rank;
     @assert num_zeros <= n * n - k_sparse
 
     # Build perspective formulation relaxation using JuMP
-    if sdp_solver == "Mosek"
-        m = Model(Mosek.Optimizer)
-        set_optimizer_attribute(m, "MSK_IPAR_LOG", solver_output)
-    else
-        m = Model(SCS.Optimizer)
-        set_optimizer_attribute(m, "verbose", solver_output)
-    end
+    m = Model(Mosek.Optimizer)
+    set_optimizer_attribute(m, "MSK_IPAR_LOG", solver_output)
 
     @variable(m, X[i=1:n, j=1:n])
     @variable(m, Y[i=1:n, j=1:n])
@@ -70,33 +70,17 @@ function solve_perspective_relaxation(U, mu, lambda, k_sparse, k_rank;
         lambda * sum(alpha[i, i] for i=1:n) +
         mu * sum(theta[i, j] for i=1:n for j=1:n))
 
+    # Solve perspective relaxation
     optimize!(m)
 
     X_sol = value.(X)
     Y_sol = value.(Y)
 
-    # Round to a feasible solution
     P_sol = value.(P)
     Z_sol = value.(Z)
     alpha_sol = value.(alpha)
     theta_sol = value.(theta)
 
-    if round_relaxed_sol
-        rounded_sol, status = round_perspective_relaxation(U, mu, lambda,
-                                                k_sparse, k_rank, Z_sol, P_sol)
-
-        if check_feasibility
-            @assert is_feasible(rounded_sol[1], rounded_sol[2],
-                                k_sparse, k_rank)
-        end
-
-        return (X_sol, Y_sol, Z_sol, P_sol), rounded_sol,
-               objective_value(m), status
-
-    else
-
-        return (X_sol, Y_sol, Z_sol, P_sol, alpha_sol), objective_value(m)
-
-    end
+    return (X_sol, Y_sol, Z_sol, P_sol, alpha_sol), objective_value(m)
 
 end;
