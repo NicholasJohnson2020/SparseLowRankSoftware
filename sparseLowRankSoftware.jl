7279a1d3ee574e91d7bd8e728da82bc87fedf277
Nicholas Johnson <>
2023-01-11 19:16:24 -0500

    small fix

--- sparseLowRankSoftware.jl
+++ sparseLowRankSoftware.jl
@@ -2,4 +2,5 @@ using LinearAlgebra, JuMP, Mosek, MosekTools, SCS, LowRankApprox, Random, Dates
 using TSVD, Distributions, StatsBase
 
 include("src/BnB.jl")
-include("src/SPCP.jl")
+include("benchmarkMethods/SPCP.jl")
+include("benchmarkMethods/AltProj.jl")
